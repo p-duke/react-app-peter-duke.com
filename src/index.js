ae781638681ec1eb2b4419e9daa4ad033b3f117c
p-duke <peteraaronduke@gmail.com>
2016-10-05 08:44:53 -0400

    added jquery to package.json

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// global.jQuery = require('jquery');
 require('bootstrap');
 require('jquery');
-// var Scrollspy = require('react-scrollspy').Scrollspy;
-// import $ from "jquery";
-// var $ = require("jquery");
 
 
 ReactDOM.render(
