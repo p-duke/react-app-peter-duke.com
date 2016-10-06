dc0d39e260686b76b3c48baf47d8e3b0a97c295d
p-duke <peteraaronduke@gmail.com>
2016-10-05 22:06:44 -0400

    updated portfolio and footer section

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,48 @@
 import React, {Component} from 'react';
 import {
   Carousel,
-  Media
+  Media,
+  Modal,
+  Button,
+  OverlayTrigger,
+  Popover,
+  Tooltip
 } from 'react-bootstrap';
 
 
 class Portfolio extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      showModal: false
+    }
+    this.close = this.close.bind(this);
+    this.open = this.open.bind(this);
+  }
+
+  close() {
+    this.setState({ showModal: false });
+  }
+
+  open() {
+    this.setState({ showModal: true });
+  }
+
+
+
+
   render() {
+    const popover = (
+      <Popover id="modal-popover" title="Devconnect">
+        Click to see more!
+      </Popover>
+    );
+    const tooltip = (
+      <Tooltip id="modal-tooltip">
+        wow.
+      </Tooltip>
+    );
     return(
       <section className="bg-portfolio" id="portfolio">
           <div className="container">
@@ -36,7 +72,8 @@ class Portfolio extends Component {
                 <Media className="col-md-4">
                     <Media.Left align="middle">
                       <div className="portfolio-item">
-                         <a href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
+                      <OverlayTrigger placement="bottom" overlay={popover}>
+                         <a onClick={this.open} href="#portfolioModal1" className="portfolio-link" data-toggle="modal">
                              <div className="caption">
                                  <div className="caption-content">
                                      <i className="fa fa-search-plus fa-3x"></i>
@@ -44,6 +81,7 @@ class Portfolio extends Component {
                              </div>
                              <img className="img-responsive" src={require('../images/mentor_search.png')} alt=""/>
                          </a>
+                         </OverlayTrigger>
                       </div>
                     </Media.Left>
                 </Media>
@@ -51,8 +89,8 @@ class Portfolio extends Component {
                   <div>
                   <Media className="col-lg-12">
                     <Media.Body>
-                      <Media.Heading>Devconnect</Media.Heading>
-                      <p>Selected as team lead for Devconnect, a hub for Dev Bootcamp graduates to connect and flourish in its thriving community plus extending the culture of learning through mentoring. An alumni can contribute to DBC's 2000+ community by posting any relevant information to our extensive forums section. The top trending and most recent forum posts will appear in a tabbed content section on the homepage. If an alumni decides to become a "mentor" they can tutor students who are seeking specific skill sets that the mentor has and later be endorsed for those skills earning badges based on a number of endorsements. The top mentors will appear on the homepage celebrating their achievements in the community. The application was built with Ruby on Rails, Python, Flask, React.js, HTML5, and Bootstrap CSS.</p>
+                      <Media.Heading><bold>Devconnect</bold></Media.Heading>
+                      <p>Selected as team lead for Devconnect, a hub for Dev Bootcamp graduates to connect and flourish in its thriving community plus extending the culture of learning through mentoring. The application was built with Ruby on Rails, Python, Flask, React.js, HTML5, and Bootstrap CSS.</p>
                     </Media.Body>
                   </Media>
                   </div>
@@ -99,6 +137,57 @@ class Portfolio extends Component {
                       </Media.Body>
                     </Media>
                 </div> */}
+
+
+        <div>
+
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Devconnect</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>User Functionality</h4>
+            <p>An alumni can contribute to DBC's 2000+ community by posting any relevant information to our extensive forums section. The top trending and most recent forum posts will appear in a tabbed content section on the homepage. If an alumni decides to become a "mentor" they can tutor students who are seeking specific skill sets that the mentor has and later be endorsed for those skills earning badges based on a number of endorsements. The top mentors will appear on the homepage celebrating their achievements in the community. The application was built with Ruby on Rails, Python, Flask, React.js, HTML5, and Bootstrap CSS.</p>
+            <hr />
+            <Carousel>
+              <Carousel.Item>
+                <img className="img-responsive center-block" width={900} height={500} alt="900x500" src={require('../images/Homepage.png')}/>
+                <Carousel.Caption className="carousel-text">
+                  <div className="overlay">
+                    <h3>Homepage</h3>
+                    <p>See what's trending including current top mentors.</p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="img-responsive center-block" width={900} height={500} alt="900x500" src={require('../images/mentor_search.png')}/>
+                <Carousel.Caption className="carousel-text">
+                  <div className="overlay">
+                    <h3>Mentor Search</h3>
+                    <p>Search for mentors that have skills you wish to learn.</p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img className="img-responsive center-block" width={900} height={500} alt="900x500" src={require('../images/user-profile.png')}/>
+                <Carousel.Caption className="carousel-text">
+                  <div className="overlay">
+                    <h3>Profile Page</h3>
+                    <p>Create and Edit your profile including skills, bio, and goals.</p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+
+
+
+
           </div>
       </section>
 
